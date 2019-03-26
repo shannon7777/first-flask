e7b71af4287f3dc23c5722db7aec05d715b0d4e2
shannon7777 <shannonsimoncherry@gmail.com>
2019-03-26 17:34:14 +0800

    added dynamic username route

--- server.py
+++ server.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 def index():
     return '<h1>Why so easy</h1>'
 
-@app.route("/another")
-def show():
-    return '<h1>YO</h1>'
+@app.route('/user/<username>')
+def show(username):
+    return f"Hi {username[0:3]}"
 
 if __name__ == '__main__':
     app.run()
