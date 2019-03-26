425e6b0afffa5392522c1314bae9a7d50c2e509e
shannon7777 <shannonsimoncherry@gmail.com>
2019-03-26 18:27:59 +0800

    added monkeypicture

--- server.py
+++ server.py
@@ -1,13 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/") # Revisit decorators if you unclear of this syntax
-def index():
-    return '<h1>Why so easy</h1>'
+@app.route("/<name>") # Revisit decorators if you unclear of this syntax
+def index(name):
+    # first_name = first_name.upper()
+    # last_name = last_name.upper()
+    name = name.upper()
+    # first_name = "shannon"
+    # last_name = "cherry"
+    # num1 = '10'
+    # num2 = '5'
+    return render_template('index.html', n = name)
 
 @app.route('/user/<username>')
 def show(username):
-    return f"Hi {username[0:3]}"
+    return f"Hi {username[5]}"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
