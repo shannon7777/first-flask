32658e0d99c4f347185ab9706d01bd9e3346a8e8
shannon7777 <shannonsimoncherry@gmail.com>
2019-03-27 11:27:15 +0800

    added new template inheritance

--- server.py
+++ server.py
@@ -1,20 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/<name>") # Revisit decorators if you unclear of this syntax
-def index(name):
-    # first_name = first_name.upper()
-    # last_name = last_name.upper()
-    name = name.upper()
-    # first_name = "shannon"
-    # last_name = "cherry"
-    # num1 = '10'
-    # num2 = '5'
-    return render_template('index.html', n = name)
 
-@app.route('/user/<username>')
-def show(username):
-    return f"Hi {username[5]}"
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
