c3257545ddb59cc115a745d61e938b7f3bf80fb4
shannon7777 <shannonsimoncherry@gmail.com>
2019-03-27 14:16:38 +0800

    use dotenv to manage environment variables

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    first_name = request.args.get('first_name')
+    return render_template('index.html', first_name=first_name)
 
 
 @app.route("/contact")
